f89d1352592dfd2dd4652fc7754423a7d0030e07
DEEKSHITH <deekshithkumar7604@gmail.com>
2025-07-31 15:32:48 +0530

    PROJECT UPLOAD

---
+++ README.md
@@ -0,0 +1,2 @@
+# M.E.R.N-Stack-Project
+M.E.R.N Stack Project Github Repository
---
+++ loginReact/src/components/SocialLogin.jsx
@@ -0,0 +1,16 @@
+const SocialLogin = () => {
+  return (
+    <div className="social-login">
+      <button className="social-button">
+        <img src="google.svg" alt="" className="social-icon" />
+        Google
+      </button>
+      <button className="social-button">
+        <img src="panda.svg" alt="" className="social-icon" />
+        Apple
+      </button>
+    </div>
+  )
+}
+
+export default SocialLogin;
